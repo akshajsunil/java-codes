53377850e5cad13fb137710c6aab8817d6ce821b
Akshaj Sunil <akshajxf@gmail.com>
2021-08-30 10:51:38 +0530

    added

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static  void main(String[] args){
+        System.out.println("Akshaj");
+        int n=5*4;
+        float n1=12;
+        String s="AKSHAJ";
+        String s1="SUNIL";
+        System.out.println(s+" "+s1);
+    }
+}
